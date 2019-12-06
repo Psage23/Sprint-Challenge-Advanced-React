1599af98b45f6c29fab93867e8a0c0c4a5f6ff56
Psage23 <pamelasage3@gmail.com>
2019-12-06 10:21:06 -0700

    done with getting player data from api

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,20 @@
 import React from 'react';
 import axios from 'axios';
+import PlayerCard from './components/PlayerCard';
 import './App.css';
 
 class App extends React.Component {
-  constructor() {
-    super();
-      this.state = {
-        player: "",
-        country: "",
-        searches: ""
+  
+      state = {
+        players: [],
       };
-  }
+  
 
   componentDidMount() {
     axios.get('http://localhost:5000/api/players')
     .then(response => {
       console.log(response);
-      this.setState({player: response.data})
+      this.setState({players: response.data})
     })
   }
 
@@ -25,13 +23,13 @@ render() {
   return (
     <div className="App">
       <h1>Women's World Cup Trends</h1>
-      <div className="name">
-        {this.state.player.name}
+      <div className="results">
+        <PlayerCard players={this.state.players}/>
       </div>
     </div>
   );
 }
-
 }
+
 
 export default App;
--- client/src/components/PlayerCard.js
+++ client/src/components/PlayerCard.js
@@ -3,9 +3,20 @@ import React from 'react'
 //this is where I want to build out the card layout for each player
 // I want to use reactstrap, need to install
 
-const PlayerCard = () => {
+const PlayerCard = props => {
     return (
-        <div></div>
+        <div>
+            {props.players.map(person => {
+                return (
+                    <div>
+                    <h1>Name: {person.name}</h1>
+                    <p>Country: {person.country}</p>
+                    <p>Searches: {person.searches}</p>
+                    </div>
+                )
+            })}
+            
+        </div>
     )
 }
 
